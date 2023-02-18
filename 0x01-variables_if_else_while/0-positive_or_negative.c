6ae6e3290f14d1af5eaa139afe300f980723b704
Odelola Boluwatife <odelolabolu2019@gmail.com>
2023-02-18 11:09:06 -0800

    Obis

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Olayide Oluwagbemiga
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints a random number and state whether
+ * it is po5sitive, negative or zero.
+ * Return: 0 on success
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+printf("%d is %s\n", n, "negative");
+else if (n > 0)
+printf("%d is %s\n", n, "positive");
+else
+printf("%d is %s\n", n, "zero");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+0x01-variables_if_else_while
