fa2f64662735a502b10c31375c5048f460427c9f
Odelola Boluwatife <odelolabolu2019@gmail.com>
2023-02-28 04:12:59 -0800

    Obis

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str:  parameter
+ *
+ * Description: A function that prints a string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
