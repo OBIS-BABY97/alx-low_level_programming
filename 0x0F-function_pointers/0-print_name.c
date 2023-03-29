65e51157ebcfc661f631c21b4f2cd1733f86273e
Odelola Boluwatife <odelolabolu2019@gmail.com>
2023-03-29 00:33:46 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,15 @@
  * print_name - function to print name
  *
  * @name: name to print
+ *
  * @f: function pointer that doesn't return anything
  *
  * Return: Always 0 (Success)
-*/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+
+if (name && f)
+f(name);
 }
